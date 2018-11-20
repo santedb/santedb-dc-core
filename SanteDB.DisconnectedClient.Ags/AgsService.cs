d82748da6ab696b514900c136399ea96b3c1541d
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-11-20 17:54:44 +0630

    Started implementation of AGS service.

---
+++ SanteDB.DisconnectedClient.Ags/AgsService.cs
@@ -0,0 +1,16 @@
+﻿using SanteDB.DisconnectedClient.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanteDB.DisconnectedClient.Ags
+{
+    /// <summary>
+    /// Represents the Applet Gateway Service
+    /// </summary>
+    public class AgsService : IDaemonService
+    {
+    }
+}
