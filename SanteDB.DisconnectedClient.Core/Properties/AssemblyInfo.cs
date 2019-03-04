9847748580ce13fe23fed9daa04c0dbae3a3efcc
Justin Fyfe <justin@fyfesoftware.ca>
2019-03-04 18:27:04 -0500

    Updated versions, updated new host context required interface properties on tests

--- SanteDB.DisconnectedClient.Core/Properties/AssemblyInfo.cs
+++ SanteDB.DisconnectedClient.Core/Properties/AssemblyInfo.cs
@@ -35,8 +35,8 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 [assembly: AssemblyInformationalVersion("Gananoque")]
-[assembly: AssemblyVersion("1.0.1.*")]
-[assembly: AssemblyFileVersion("0.9.9")]
+[assembly: AssemblyVersion("1.20.0.*")]
+[assembly: AssemblyFileVersion("1.20.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
