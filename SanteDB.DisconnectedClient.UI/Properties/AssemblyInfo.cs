63051675bbdfafbf96edb7b0e9eb1c9ef85a8e16
Justin Fyfe <justin@fyfesoftware.ca>
2019-09-03 18:31:46 +0700

    Release of version 1.68

--- SanteDB.DisconnectedClient.UI/Properties/AssemblyInfo.cs
+++ SanteDB.DisconnectedClient.UI/Properties/AssemblyInfo.cs
@@ -35,9 +35,9 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.67.0.*")]
+[assembly: AssemblyVersion("1.68.0.*")]
 [assembly: AssemblyInformationalVersion("Halifax")]
-[assembly: AssemblyFileVersion("1.67.0.0")]
+[assembly: AssemblyFileVersion("1.68.0.0")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 
