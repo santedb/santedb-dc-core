e7743e92a16adec3f42e412646982fbb478ac3f2
Justin Fyfe <justin@fyfesoftware.ca>
2020-02-26 19:40:24 -0500

    Updated version codes

--- SanteDB.DisconnectedClient.UI/DcApplicationContext.cs
+++ SanteDB.DisconnectedClient.UI/DcApplicationContext.cs
@@ -29,7 +29,7 @@ using SanteDB.Core.Services;
 using SanteDB.DisconnectedClient.Core;
 using SanteDB.DisconnectedClient.Core.Configuration;
 using SanteDB.DisconnectedClient.Core.Configuration.Data;
-using SanteDB.DisconnectedClient.Core.Data;
+//using SanteDB.DisconnectedClient.Core.Data;
 using SanteDB.DisconnectedClient.Core.Services;
 using SanteDB.DisconnectedClient.i18n;
 using SanteDB.DisconnectedClient.Xamarin;
