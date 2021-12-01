fb160acf86af21e02be370a6125ce891aed6a550
Justin Fyfe <justin@fyfesoftware.ca>
2021-11-30 22:50:46 -0500

    Updated the remote audit repository service to use the dispatcher queue services so that down central servers don't flood the thread pool

--- SanteDB.DisconnectedClient.Core/Services/Remote/RemoteAuditRepositoryService.cs
+++ SanteDB.DisconnectedClient.Core/Services/Remote/RemoteAuditRepositoryService.cs
@@ -1,38 +1,46 @@
 ﻿/*
  * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors (See NOTICE.md)
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: fyfej
  * Date: 2021-2-9
  */
+
 using SanteDB.Core.Auditing;
 using SanteDB.Core.Diagnostics;
 using SanteDB.Core.Model.AMI.Security;
 using SanteDB.Core.Model.Query;
+using SanteDB.Core.Queue;
 using SanteDB.Core.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading;
 
 namespace SanteDB.DisconnectedClient.Services.Remote
 {
     /// <summary>
     /// Represents a remote audit repository service that usses the AMI to communicate audits
     /// </summary>
-    public class RemoteAuditRepositoryService : AmiRepositoryBaseService, IRepositoryService<AuditData>
+    public class RemoteAuditRepositoryService : AmiRepositoryBaseService, IRepositoryService<AuditData>, IDisposable
     {
+        // AMI queue name
+        private const string AmiQueueName = "sys.ami";
+
+        // Dead letter queue
+        private const string AmiDeadQueueName = "sys.ami.dead";
 
         /// <summary>
         /// Get the service name
@@ -42,12 +50,48 @@ namespace SanteDB.DisconnectedClient.Services.Remote
         // Get a tracer
         private Tracer m_tracer = Tracer.GetTracer(typeof(RemoteAuditRepositoryService));
 
+        // Background sender thread
+        private readonly IDispatcherQueueManagerService m_dispatcherQueue;
+
+        /// <summary>
+        /// Remote audit repository service
+        /// </summary>
+        public RemoteAuditRepositoryService(IDispatcherQueueManagerService dispatcherQueueManagerService, IAuditDispatchService auditDispatchService = null)
+        {
+            this.m_dispatcherQueue = dispatcherQueueManagerService;
+
+            this.m_dispatcherQueue.Open(AmiQueueName);
+            this.m_dispatcherQueue.Open(AmiDeadQueueName);
+            this.m_dispatcherQueue.SubscribeTo(AmiQueueName, this.MonitorOutboundQueue);
+        }
+
+        /// <summary>
+        /// Monitor the outbound queue
+        /// </summary>
+        private void MonitorOutboundQueue(DispatcherMessageEnqueuedInfo enqueuedInfo)
+        {
+            using (var client = this.GetClient())
+            {
+                while ((this.m_dispatcherQueue.Dequeue(AmiQueueName)?.Body is AuditData audit))
+                {
+                    try
+                    {
+                        client.SubmitAudit(new AuditSubmission(audit));
+                    }
+                    catch (Exception e)
+                    {
+                        this.m_tracer.TraceError("Error dispatching audit: {0}", e);
+                        this.m_dispatcherQueue.Enqueue(AmiDeadQueueName, audit);
+                    }
+                }
+            }
+        }
+
         /// <summary>
         /// Find the specified audit data
         /// </summary>
         public IEnumerable<AuditData> Find(Expression<Func<AuditData, bool>> query)
         {
-
             int tr = 0;
             return this.Find(query, 0, null, out tr);
         }
@@ -77,13 +121,12 @@ namespace SanteDB.DisconnectedClient.Services.Remote
         /// </summary>
         public AuditData Insert(AuditData audit)
         {
-            using (var client = this.GetClient())
-                client.SubmitAudit(new AuditSubmission(audit));
+            this.m_dispatcherQueue.Enqueue(AmiQueueName, audit);
             return audit;
         }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <param name="key"></param>
         /// <param name="versionKey"></param>
@@ -94,12 +137,11 @@ namespace SanteDB.DisconnectedClient.Services.Remote
         }
 
         /// <summary>
-        /// Update the specified audit data 
+        /// Update the specified audit data
         /// </summary>
         public AuditData Save(AuditData data)
         {
-            using (var client = this.GetClient())
-                client.SubmitAudit(new AuditSubmission(data));
+            this.m_dispatcherQueue.Enqueue(AmiQueueName, data);
             return data;
         }
 
@@ -109,6 +151,14 @@ namespace SanteDB.DisconnectedClient.Services.Remote
         public AuditData Obsolete(Guid key)
         {
             throw new NotImplementedException("Remote audits cannot be deleted");
+        }
+
+        /// <summary>
+        /// Dispose of the object
+        /// </summary>
+        public void Dispose()
+        {
+            this.m_dispatcherQueue.UnSubscribe(AmiQueueName, this.MonitorOutboundQueue);
         }
     }
 }
