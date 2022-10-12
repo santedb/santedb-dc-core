6742097a395b48fb7d6a57ee506e133f6bf57ffa
Justin Fyfe <justin@fyfesoftware.ca>
2022-10-12 12:15:09 -0400

    Migrated the services for applet manager for debug

---
+++ SanteDB.Disconnected/Services/IAppletHostBridgeProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SanteDB.Disconnected.Services
+{
+    /// <summary>
+    /// Provides the SanteDB application gateway which binds the JavaScript
+    /// layer with the host context
+    /// </summary>
+    public interface IAppletHostBridgeProvider
+    {
+
+        /// <summary>
+        /// Get the bridge script which allows the client side JavaScript to 
+        /// interact with the host
+        /// </summary>
+        String GetBridgeScript();
+    }
+}
