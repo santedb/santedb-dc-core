275635f6443ec02137d44fe8f9deef8bd7fa2d43
Trevor Davis <trevor@santesuite.com>
2022-11-10 07:33:44 -0500

    Stubbed Synchronization Objects.

---
+++ SanteDB.Client.Disconnected/Data/Synchronization/DefaultSynchronizationLogService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SanteDB.Client.Disconnected.Data.Synchronization
+{
+    public class DefaultSynchronizationLogService : ISynchronizationLogService
+    {
+        public void CompleteQuery(Guid queryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(ISynchronizationLogEntry itm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISynchronizationLogQuery FindQueryData(Type modelType, string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ISynchronizationLogEntry> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetLastEtag(Type modelType, string filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime? GetLastTime(Type modelType, string filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Type modelType, string filter, string eTag, string name, DateTime since)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveQuery(Type modelType, string filter, Guid queryId, string name, int offset)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SanteDB.Client.Disconnected/Data/Synchronization/DefaultSynchronizationQueueManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SanteDB.Client.Disconnected.Data.Synchronization
+{
+    public class DefaultSynchronizationQueueManager : ISynchronizationQueueManager
+    {
+        public string ServiceName => "Synchronization Queue Manager";
+
+        public ISynchronizationQueue Get(string queueName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ISynchronizationQueue> GetAll(SynchronizationPattern queueType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
