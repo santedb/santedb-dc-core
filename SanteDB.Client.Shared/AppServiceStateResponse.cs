9bff8a60168f1081ef852a1da84bf7660afb7359
Trevor Davis <trevor@trevordavis.me>
2023-03-10 10:06:02 -0500

    Add shared project for client applications.

---
+++ SanteDB.Client.Shared/AppServiceStateResponse.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanteDB.Client.Shared
+{
+    public class AppServiceStateResponse
+    {
+        [JsonProperty("version")]
+        public string? Version { get; set; }
+        [JsonProperty("online")]
+        public bool Online { get; set; }
+        [JsonProperty("hdsi")]
+        public bool Hdsi { get; set; }
+        [JsonProperty("ami")]
+        public bool Ami { get; set; }
+        [JsonProperty("client_id")]
+        public string? ClientId { get; set; }
+        [JsonProperty("device_id")]
+        public string? DeviceId { get; set; }
+        [JsonProperty("realm")]
+        public string? Realm { get; set; }
+        [JsonProperty("magic")]
+        public string? Magic { get; set; }
+    }
+}
