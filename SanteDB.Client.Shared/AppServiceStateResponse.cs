6e20df9c4cc81a21da1527eb4eced36954fb734d
Trevor Davis <trevor@santesuite.com>
2024-01-05 13:25:23 -0500

    Added license header to source files.

--- SanteDB.Client.Shared/AppServiceStateResponse.cs
+++ SanteDB.Client.Shared/AppServiceStateResponse.cs
@@ -1,4 +1,24 @@
-﻿using Newtonsoft.Json;
+﻿/*
+ * Copyright (C) 2021 - 2024, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
+ * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
+ * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ * User: trevor
+ * Date: 2023-4-19
+ */
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,22 +27,49 @@ using System.Threading.Tasks;
 
 namespace SanteDB.Client.Shared
 {
+    /// <summary>
+    /// A serialization class for the response of app state for consumption by the web runtime.
+    /// </summary>
     public class AppServiceStateResponse
     {
+        /// <summary>
+        /// The app version
+        /// </summary>
         [JsonProperty("version")]
         public string? Version { get; set; }
+        /// <summary>
+        /// True when the runtime can access an upstream realm.
+        /// </summary>
         [JsonProperty("online")]
         public bool Online { get; set; }
+        /// <summary>
+        /// True when the upstream HDSI service is available.
+        /// </summary>
         [JsonProperty("hdsi")]
         public bool Hdsi { get; set; }
+        /// <summary>
+        /// True when the upstream AMI service is available.
+        /// </summary>
         [JsonProperty("ami")]
         public bool Ami { get; set; }
+        /// <summary>
+        /// The app client id for security operations.
+        /// </summary>
         [JsonProperty("client_id")]
         public string? ClientId { get; set; }
+        /// <summary>
+        /// The device id for security operations.
+        /// </summary>
         [JsonProperty("device_id")]
         public string? DeviceId { get; set; }
+        /// <summary>
+        /// When part of an upstream realm, the realm that this app is part of.
+        /// </summary>
         [JsonProperty("realm")]
         public string? Realm { get; set; }
+        /// <summary>
+        /// The unique random identifier to provide a security safeguard against unauthorized use of the app.
+        /// </summary>
         [JsonProperty("magic")]
         public string? Magic { get; set; }
     }
--- SanteDB.Client.Shared/LocalAppDirectoryProvider.cs
+++ SanteDB.Client.Shared/LocalAppDirectoryProvider.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/*
+ * Copyright (C) 2021 - 2024, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
+ * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
+ * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ * User: trevor
+ * Date: 2023-4-19
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,21 +26,22 @@ using System.Threading.Tasks;
 
 namespace SanteDB.Client.Shared
 {
+    /// <summary>
+    /// The directory provider that will provide directories for storing data and configuration.
+    /// </summary>
     public class LocalAppDirectoryProvider
     {
         readonly string m_AppDirectory;
         readonly string m_DataDirectory;
         readonly string m_ConfigDirectory;
 
-        public LocalAppDirectoryProvider()
-            : this("dc-win32")
+        /// <summary>
+        /// Instantiates a new instance of the provider.
+        /// </summary>
+        /// <param name="appName">The name of the app that will be used as part of the path for the configuration and data.</param>
+        public LocalAppDirectoryProvider(string appName)
         {
-
-        }
-
-        public LocalAppDirectoryProvider(string directoryName)
-        {
-            m_AppDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "SanteDB", directoryName);
+            m_AppDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "SanteDB", appName);
             m_ConfigDirectory = Path.Combine(m_AppDirectory, "config");
             m_DataDirectory = Path.Combine(m_AppDirectory, "data");
 
@@ -28,6 +49,12 @@ namespace SanteDB.Client.Shared
             AppDomain.CurrentDomain.SetData("ConfigDirectory", m_ConfigDirectory);
         }
 
+        /// <summary>
+        /// Will attempt to create any directories that do not exist. 
+        /// </summary>
+        /// <remarks>
+        /// This does not catch any exceptions that could occur. This is to ensure a successful call indicates the directories will exist. 
+        /// </remarks>
         private void EnsureDirectoriesAreCreated()
         {
             Directory.CreateDirectory(m_AppDirectory);
@@ -35,6 +62,10 @@ namespace SanteDB.Client.Shared
             Directory.CreateDirectory(m_DataDirectory);
         }
 
+        /// <summary>
+        /// Retrieves the configuration file path and file name.
+        /// </summary>
+        /// <returns>The full path for the configuration file.</returns>
         public string GetConfigFilePath()
         {
             EnsureDirectoriesAreCreated();
@@ -43,13 +74,20 @@ namespace SanteDB.Client.Shared
         }
 
         
-
+        /// <summary>
+        /// Retrieves the configuration directory.
+        /// </summary>
+        /// <returns>The directory path for the configuration directory.</returns>
         public string GetConfigDirectory()
         {
             EnsureDirectoriesAreCreated();
             return m_ConfigDirectory;
         }
 
+        /// <summary>
+        /// Retrieves the data directory.
+        /// </summary>
+        /// <returns>The directory path for the data directory.</returns>
         public string GetDataDirectory()
         {
             EnsureDirectoriesAreCreated();
