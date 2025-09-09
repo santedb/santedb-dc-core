d27769956fe31675c742cc2846ad94c351d8853a
Justin Fyfe <justin@fyfesoftware.ca>
2025-09-09 14:07:49 -0400

    Added fallback to generic language

--- SanteDB.Client/UserInterface/WebAppletHostBridgeProvider.cs
+++ SanteDB.Client/UserInterface/WebAppletHostBridgeProvider.cs
@@ -68,7 +68,7 @@ namespace SanteDB.Client.UserInterface
                     sw.WriteLine("__SanteDBAppService.GetMagic = function() {{ return '{0}'; }}", ApplicationServiceContext.Current.ActivityUuid.ToByteArray().HexEncode());
                     sw.WriteLine("__SanteDBAppService.GetVersion = function() {{ return '{0} ({1})'; }}", Assembly.GetEntryAssembly().GetName().Version, Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion);
                     sw.WriteLine("__SanteDBAppService.GetString = function(key) {");
-                    sw.WriteLine("\tvar strData = __SanteDBAppService._stringData[__SanteDBAppService.GetLocale()] || __SanteDBAppService._stringData['en'];");
+                    sw.WriteLine("\tvar strData = __SanteDBAppService._stringData[__SanteDBAppService.GetLocale()] || __SanteDBAppService._stringData[/^([a-z]{2})-[a-z]{2}$/i.exec(__SanteDBAppService.GetLocale())[1]] || __SanteDBAppService._stringData['en'];");
                     sw.WriteLine("\treturn strData[key] || key;");
                     sw.WriteLine("}");
 
